5082ae94822fc99674bf16fe9df4544dcfbfaa75
Walter Shewmake <waltershewmake@gmail.com>
2023-11-01 14:49:17 -0600

    initla commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { type AppType } from "next/app";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <MantineProvider withGlobalStyles withNormalizeCSS>
+     <Component {...pageProps} />
+  </MantineProvider>
+};
+
+export default api.withTRPC(MyApp);
